54323739937a900bafd5f8cd07e500372b3293a6
Rogue1-cmd <garipeter01@gmail.com>
2025-03-13 10:34:14 +0300

    tcpchatroom

---
+++ tcpchatroom/server.py
@@ -0,0 +1,39 @@
+import threading
+import socket
+
+host = '127.0.0.1'
+port = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(host, port)
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            broadcast(f'{nickname} ameleft!!'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print("connected with {str(address)}")
+
+
+
