bf569a3ba8c98726e6be126918e122177324f236
Rogue1-cmd <garipeter01@gmail.com>
2025-03-23 21:59:10 +0300

    -----

--- tcpchatroom/client.py
+++ tcpchatroom/client.py
@@ -6,6 +6,7 @@ nickname = input ("Choose a nickname: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 55555))
 
+# This function receives messages from the server
 def receive():
     while True:
         try:
@@ -18,7 +19,8 @@ def receive():
             print("An error occurred!")
             client.close()
             break
-
+# This function sends messages to the server
+#continuously prompts the user to input a message, prefixes it with their nickname, and sends it to the server via a socket connection
 def write():
     while True:
         message = f'{nickname}: {input("")}'
